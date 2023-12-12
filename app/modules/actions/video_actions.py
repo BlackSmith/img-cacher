ddca0066caaed1f5a13c7f7c8941f1413f8df647
Martin Korbel <korbel@blackserver.cz>
2023-12-12 16:59:10 +0100

    Fix video reize II

--- app/modules/actions/video_actions.py
+++ app/modules/actions/video_actions.py
@@ -50,7 +50,7 @@ class VideoActions(Action):
 
         video_clip = VideoFileClip(image.full_path)
         gif_clip = video_clip.subclip(0, 5) \
-            .fx(fx_resize, (a_image.width, a_image.height))
+            .fx(fx_resize.resize, (a_image.width, a_image.height))
         logger.info(
             f"The video {image.filename} was resized "
             f"({a_image.width}x{a_image.height}) to {a_image.filename}")
